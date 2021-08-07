eaa622fe82cf837d552bb328212af929bb28c4ff
Levon Ritter <levong2112@gmail.com>
2021-08-07 19:42:32 -0400

    added button group

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
 import SaveIcon from '@material-ui/icons/Save';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 
 
@@ -9,15 +11,24 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Button size="small"
-        startIcon={<SaveIcon />}
-        endIcon={<SaveIcon />}
-        style={{fontSize:12}}
-        onClick={() => alert('hello')} 
-        variant="contained" 
-        color="primary">
-          Hello World
-        </Button>
+        <ButtonGroup variant="contained" 
+          color="primary">>
+          <Button size="small"
+          startIcon={<SaveIcon />}
+          endIcon={<SaveIcon />}
+          style={{fontSize:12}}
+          onClick={() => alert('hello')} >
+          
+            Save
+          </Button>
+          <Button size="small"
+          startIcon={<DeleteIcon />}
+          endIcon={<DeleteIcon  />}>
+
+            Discard
+          </Button>
+        </ButtonGroup>
+       
         <img src={logo} className="App-logo" alt="logo" />
 
       </header>

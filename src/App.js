57598184b4df5da02fbdbf88d5e84dc00357c7e7
Levon Ritter <levong2112@gmail.com>
2021-08-13 16:58:26 -0400

    added a grid and paper

--- src/App.js
+++ src/App.js
@@ -10,6 +10,12 @@ import DeleteIcon from '@material-ui/icons/Delete';
 import TextField from '@material-ui/core/TextField';
 import { makeStyles, ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import { green, orange } from '@material-ui/core/colors';
+import 'fontsource-roboto';
+import Typography from '@material-ui/core/Typography';
+import Container  from '@material-ui/core/Container';
+
+import Paper  from '@material-ui/core/Paper';
+import Grid  from '@material-ui/core/Grid';
 
 const useStyles = makeStyles({
   root:{
@@ -22,6 +28,12 @@ const useStyles = makeStyles({
 })
 
 const theme = createMuiTheme({
+  typography: {
+    h2: {
+      fontSize: 38,
+      marginBottom: 15,
+    }
+  },
   palette: {
     primary: {
       main: orange[500],
@@ -65,9 +77,37 @@ function App() {
     
 
     <ThemeProvider theme={theme}>
-            <div className="App">
+      <Container maxWidth='xl'>
+      <div className="App">
       <header className="App-header">
+        <Typography variant="h2" component="div">
+          Welcome to MUI
+          Div component
+        </Typography>
+        <Typography variant="h3">
+          Learn material UI
+        </Typography>
+        <Typography variant="subtitle1">
+          subtitle1
+        </Typography>
+        <Typography variant="body1">
+          body1
+        </Typography>
         <ButtonStyled></ButtonStyled>
+        <Grid container spacing={3} justify="center">
+          <Grid item xs={3} sm={6}>
+            <Paper style={{height:75, width:'100%'}}>
+            </Paper>
+          </Grid>
+          <Grid item xs={3} sm={6}>
+            <Paper style={{height:75, width:'100%'}}>
+            </Paper>
+          </Grid>
+          <Grid item xs={3} sm={6}>
+            <Paper style={{height:75, width:'100%'}}>
+            </Paper>
+          </Grid>
+        </Grid>      
         <TextField
           variant="outlined"
           color="secondary"
@@ -82,7 +122,7 @@ function App() {
         />
         <CheckboxExample/>
         <ButtonGroup variant="contained" 
-          color="primary">>
+          color="primary">
           <Button size="small"
           startIcon={<SaveIcon />}
           endIcon={<SaveIcon />}
@@ -106,6 +146,8 @@ function App() {
 
       </header>
     </div>
+      </Container>
+     
     </ThemeProvider>
 
     

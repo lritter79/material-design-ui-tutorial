7716e29b0f2c13cc1e90922c53c2c0af147b8f98
Levon Ritter <levong2112@gmail.com>
2021-08-13 15:34:43 -0400

    added styled button

--- src/App.js
+++ src/App.js
@@ -8,6 +8,26 @@ import ButtonGroup from '@material-ui/core/ButtonGroup';
 import SaveIcon from '@material-ui/icons/Save';
 import DeleteIcon from '@material-ui/icons/Delete';
 import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  root:{
+    background: 'linear-gradient(45deg, #333, #999)',
+    border:0,
+    borderRadius:15,
+    color:'white',
+    padding: '0 30px'
+  }
+})
+
+function ButtonStyled(){
+  const classes = useStyles()
+  return (
+    <Button className={classes.root}>
+      Test Styled Button
+    </Button>
+  )  
+}
 
 function CheckboxExample() {
   const [checked, setChecked] = useState(true)
@@ -32,6 +52,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <ButtonStyled></ButtonStyled>
         <TextField
           variant="outlined"
           color="secondary"
